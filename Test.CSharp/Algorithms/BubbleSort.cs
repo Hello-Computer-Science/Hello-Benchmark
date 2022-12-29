5419ecc6c10db08fa35aacc2052f250b74203fb1
Dynesshely <catrol@qq.com>
2022-12-30 01:00:54 +0800

    💾 Feat: C# 版本 冒泡排序

---
+++ Test.CSharp/Algorithms/BubbleSort.cs
@@ -0,0 +1,20 @@
+namespace Test.CSharp.Algorithms;
+
+public class BubbleSort
+{
+    public static void Sort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
--- Test.CSharp/Program.cs
+++ Test.CSharp/Program.cs
@@ -1,6 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Test.CSharp.Algorithms;
 
+namespace Test.CSharp;
 
+#region BubbleSort
+
+const int testLevel = 100_000_000;
+int[] array = new int[testLevel];
+
+#endregion
 
 
